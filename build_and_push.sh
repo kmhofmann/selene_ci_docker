d27f78eba33ccf50b24c335afe9a7e870ceb0770
Michael Hofmann <kmhofmann@gmail.com>
2018-12-11 16:24:17 +0100

    Push base Docker image after successful build.

--- build_and_push.sh
+++ build_and_push.sh
@@ -6,8 +6,8 @@ set -e
 set -x
 
 docker build -t kmhofmann/selene:test_base ${SCRIPT_DIR}/test_base/
-docker build -t kmhofmann/selene:test_apt ${SCRIPT_DIR}/test_apt/
-docker build -t kmhofmann/selene:test_vcpkg ${SCRIPT_DIR}/test_vcpkg/
 docker push kmhofmann/selene:test_base
+docker build -t kmhofmann/selene:test_apt ${SCRIPT_DIR}/test_apt/
 docker push kmhofmann/selene:test_apt
+docker build -t kmhofmann/selene:test_vcpkg ${SCRIPT_DIR}/test_vcpkg/
 docker push kmhofmann/selene:test_vcpkg
