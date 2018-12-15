1a275dc65d558bd23410f529d2b26ab6233a058e
Michael Hofmann <kmhofmann@gmail.com>
2018-12-15 19:57:43 +0100

    Enable vcpkg build with Clang/libc++.

--- build_and_push.sh
+++ build_and_push.sh
@@ -5,9 +5,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 set -e
 set -x
 
-docker build -t kmhofmann/selene:test_base ${SCRIPT_DIR}/test_base/
-docker push kmhofmann/selene:test_base
-docker build -t kmhofmann/selene:test_apt ${SCRIPT_DIR}/test_apt/
-docker push kmhofmann/selene:test_apt
-docker build -t kmhofmann/selene:test_vcpkg ${SCRIPT_DIR}/test_vcpkg/
-docker push kmhofmann/selene:test_vcpkg
+docker build -t kmhofmann/selene_test_base ${SCRIPT_DIR}/test_base/
+docker push kmhofmann/selene_test_base
+docker build -t kmhofmann/selene_test_apt ${SCRIPT_DIR}/test_apt/
+docker push kmhofmann/selene_test_apt
+docker build -t kmhofmann/selene_test_vcpkg ${SCRIPT_DIR}/test_vcpkg/
+docker push kmhofmann/selene_test_vcpkg
